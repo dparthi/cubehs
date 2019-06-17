316b198881378da72e30c4bfeeedff2ac7598b1e
aravind mallapureddy <aravind.mallapureddy@juspay.in>
2019-06-17 12:24:25 +0530

    removed nix pinning in shell.nix as mac os is not working with it

--- shell.nix
+++ shell.nix
@@ -1,27 +1,28 @@
+{ nixpkgs ? import <nixpkgs> {} }:
 let
-  inherit (import <nixpkgs> {}) fetchFromGitHub;
-  nixpkgs = fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "19.03";
-    sha256 = "0q2m2qhyga9yq29yz90ywgjbn9hdahs7i8wwlq7b55rdbyiwa5dy";
-  };
-  pkgs = import nixpkgs {};
+  
+  inherit (nixpkgs) pkgs;
+  inherit (pkgs) haskellPackages;
+
+  haskellDeps = ps: with ps; [
+    base
+    gloss
+    stylish-haskell
+    hindent
+    hlint
+    ghcid
+  ];
+
+  ghc = haskellPackages.ghcWithPackages haskellDeps;
+
+  nixPackages = [
+    ghc
+    pkgs.gdb
+    haskellPackages.cabal-install
+    haskellPackages.cabal2nix
+  ];
 in
 pkgs.stdenv.mkDerivation {
   name = "haskell-cube";
-  buildInputs = [
-    (pkgs.haskell.packages.ghc864.ghcWithHoogle (hps:
-      with hps;
-      [
-        # libraries
-        ghcid
-        stylish-haskell
-        hindent
-        hlint
-        cabal-install
-        gloss
-      ]))
-    pkgs.haskellPackages.cabal2nix
-  ];
+  buildInputs = nixPackages;
 }
