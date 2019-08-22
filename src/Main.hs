ffe0ea0db45b884560fbd88d34c2189310aa1a8f
Parthiban Duraisamy <parthiban.duraisamy@juspay.in>
2019-08-22 10:15:42 +0530

    new approach using edges

--- src/Main.hs
+++ src/Main.hs
@@ -4,24 +4,32 @@ import Graphics.Gloss
 main = display (InWindow "Nice Window" (200, 200) (10, 10)) white (renderCube [])
 -- main = print initialPointTemplate
 
-type Edge = [Point]
+type Edge = (Point3D,Point3D)
 type Cube = [Edge]
+type Point3D = (Float, Float, Float)
 
-p1,p2,p3,p4,p5,p6,p7,p8 :: Point
-p1 = ((-1),1)
-p2 = (1,1)
-p3 = (1,(-1))
-p4 = ((-1),(-1))
-p5 = ((-1),1)
-p6 = (1,1)
-p7 = (1,(-1))
-p8 = ((-1),(-1))
-
-initialPoints :: [Point]
-initialPoints = [p1,p2,p3,p4,p5,p6,p7,p8]
-
-initialEdges :: [Edge]
-initialEdges = [] 
+getCube :: [Point3D] -> Cube
+getCube coordinates = [(c1, c2)
+                        , (c2, c3)
+                        , (c3, c4)
+                        , (c4, c1)
+                        , (c1, c5)
+                        , (c5, c6)
+                        , (c6, c7)
+                        , (c7, c8)
+                        , (c8, c5)
+                        , (c2, c6)
+                        , (c3, c7)
+                        , (c4, c8)]
+        where c1 = coordinates !! 0
+              c2 = coordinates !! 1
+              c3 = coordinates !! 2
+              c4 = coordinates !! 3
+              c5 = coordinates !! 4
+              c6 = coordinates !! 5
+              c7 = coordinates !! 6
+              c8 = coordinates !! 7
 
 renderCube :: Cube -> Picture
 renderCube _ = Circle 80
+
