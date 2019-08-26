c586d8b44a2f315246499387ecf30a9c9e41b72f
Parthiban Duraisamy <parthiban.duraisamy@juspay.in>
2019-08-26 14:35:55 +0530

    added keyboard based rotation

--- src/Main.hs
+++ src/Main.hs
@@ -11,22 +11,31 @@ import Graphics.Gloss.Interface.IO.Interact
 import qualified Cube as Cube
 -- main = display (InWindow "Nice Window" (200, 200) (10, 10)) white (renderModel [])
 -- main = simulate FullScreen blue 1 initModel renderModel rotateCubeOnAlternateAxes
-main = play FullScreen blue 1 initModel renderModel eventHandler rotateCubeOnAlternateAxes'
+main = play FullScreen blue 1 initModel renderModel eventHandler step
 
-data Model = Model { cube:: Cube.Cube, lastAxis::Cube.Axis } deriving Show
+data Model = Model { cube:: Cube.Cube } deriving Show
 initModel :: Model
-initModel = Model { cube = Cube.getCube initialCoordinates, lastAxis = Cube.X }
+initModel = Model { cube = Cube.getCube initialCoordinates}
+
+step :: Float -> Model -> Model
+step _ model = model
 
 renderModel :: Model -> Picture
 renderModel model = mconcat $ map (\x -> Line $ Cube.getLine x) $ cube model
 
-rotateCubeOnAlternateAxes' :: Float -> Model -> Model
-rotateCubeOnAlternateAxes' _ model 
-  | lastAxis model == Cube.X = rotateModel model Cube.Y $ pi/4
-  | lastAxis model == Cube.Y = rotateModel model Cube.X $ pi/4
-
 rotateModel :: Model -> Cube.Axis -> Float -> Model
-rotateModel model axis angle = Model {cube = Cube.rotateCube (cube model) axis angle, lastAxis = axis}
+rotateModel model axis angle = Model {cube = Cube.rotateCube (cube model) axis angle}
+
+step_angle :: Float
+step_angle = 0.1
+
+eventHandler :: Event -> Model -> Model
+eventHandler (EventKey (MouseButton LeftButton) Down xPos yPos) model = model
+eventHandler (EventKey (Char 'a') _ _ _) model = Model {cube = Cube.rotateCube (cube model) Cube.Y ((-1) * step_angle)}
+eventHandler (EventKey (Char 'd') _ _ _) model = Model {cube = Cube.rotateCube (cube model) Cube.Y step_angle}
+eventHandler (EventKey (Char 's') _ _ _) model = Model {cube = Cube.rotateCube (cube model) Cube.X step_angle}
+eventHandler (EventKey (Char 'w') _ _ _) model = Model {cube = Cube.rotateCube (cube model) Cube.X ((-1) * step_angle)}
+eventHandler _ model = model
 
 initialCoordinates = [((-120),120,(-120))
                       , (120,120,(-120))
@@ -37,10 +46,3 @@ initialCoordinates = [((-120),120,(-120))
                       , (120,(-120),120)
                       , ((-120),(-120),120)
                       ]
-
-eventHandler :: Event -> Model -> Model
-eventHandler (EventKey (MouseButton LeftButton) Down xPos yPos) model = model
-eventHandler _ model = model
-
-rotateCubeOnAlternateAxes :: ViewPort -> Float -> Model -> Model
-rotateCubeOnAlternateAxes _ x model = rotateCubeOnAlternateAxes' x model
